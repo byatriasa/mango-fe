b8ebeebdc037baf8bbbf26e2157d1302bc0c7c15
Byatriasa <byatrsaa@gmail.com>
2024-06-13 00:13:10 +0700

    feat: add customize error message for validators

--- src/validators/applicant.validator.ts
+++ src/validators/applicant.validator.ts
@@ -2,8 +2,11 @@ import z from 'zod'
 
 export type Register = z.infer<typeof register>
 export const register = z.object({
-  name: z.string(),
-  identity_number: z.string().length(16).regex(/^\d+$/),
-  email: z.string().email(),
-  dob: z.string().date()
+  name: z.string().min(1, 'Name must not be empty'),
+  identity_number: z
+    .string()
+    .length(16, 'Identity number must be a number and 16 characters long')
+    .regex(/^\d+$/, 'Identity number must be a number and 16 characters long'),
+  email: z.string().email('Invalid email address'),
+  dob: z.string().date('Invalid date of birth')
 })
